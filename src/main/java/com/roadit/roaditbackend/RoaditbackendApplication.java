405f8ba57d32d699eed6bffea99214e1e8997d14
JIYOON <ejiyoon37@email.com>
2025-04-02 20:07:00 +0900

    Inital commit

---
+++ src/main/java/com/roadit/roaditbackend/RoaditbackendApplication.java
@@ -0,0 +1,13 @@
+package com.roadit.roaditbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoaditbackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoaditbackendApplication.class, args);
+	}
+
+}
