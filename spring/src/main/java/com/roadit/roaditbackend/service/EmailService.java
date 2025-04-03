9d1e0d100f8e3613012682587e9eaac199fbafa2
Dezeli <haterecursive@gmail.com>
2025-04-03 16:03:23 +0900

    Email_verification

--- spring/src/main/java/com/roadit/roaditbackend/service/EmailService.java
+++ spring/src/main/java/com/roadit/roaditbackend/service/EmailService.java
@@ -1,3 +1,4 @@
+package com.roadit.roaditbackend.service;
 import lombok.RequiredArgsConstructor;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
